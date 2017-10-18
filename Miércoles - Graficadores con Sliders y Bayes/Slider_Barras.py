13b0ce1d48984a8a97503c55fed1736f2fc3d8dd
Adriana Chavez <adrifelcha@gmail.com>
2017-10-18 16:59:26 -0500

    Homologando las graficas

--- Miércoles - Graficadores con Sliders y Bayes/Slider_Barras.py
+++ Miércoles - Graficadores con Sliders y Bayes/Slider_Barras.py
@@ -11,8 +11,7 @@ Curso: 'Un laboratorio virtual en Python para modelos del comportamiento'
     #Ejecutar -> Configurar -> Ejecutar en una terminal de Python dedicada...
     
     http://html-color-codes.info/            <- Sitio recomendado para obtener codigos HTML por color"""
-
-    
+   
 #######################################################################################################
 #######################################################################################################
 """Cargamos nuestras librerias"""
@@ -29,10 +28,10 @@ fig, ax = plt.subplots()                           #Ampliamos nuestro espacio pa
 plt.subplots_adjust(left=0.1, bottom=0.25)         #Especificamos los márgenes de nuestras figuras respecto de la ventana
 
 """Dibujamos BARRAS"""                
-b_Ana = ax.bar(0.4, Alumnos[0], facecolor='#48B21E', width=0.3)      #Dibujamos la primer barra para Ana
-b_Pepe = ax.bar(1, Alumnos[1], facecolor='#1E1EB2', width=0.3)        #Dibujamos una segunda barra para Pepe
+b_Ana = ax.bar(0.3, Alumnos[0], facecolor='#48B21E', width=0.5)      #Dibujamos la primer barra para Ana
+b_Pepe = ax.bar(1, Alumnos[1], facecolor='#1E1EB2', width=0.5)        #Dibujamos una segunda barra para Pepe
         #La función bar (Barra) tiene los siguientes argumentos:  (Ubicación en x, valor a graficar, color, ancho)
-ax.axis([0.01, 1.4, 0.0, 10.15])                                   #Especificamos las dimensiones de nuestra gráfica
+ax.axis([0.01, 1.7, 0.0, 10.15])                                   #Especificamos las dimensiones de nuestra gráfica
      #La función axis (Dimensión de los ejes) tiene los siguientes argymentos:
      #los primeros dos valores corresponden al rango abarcado en el eje X y los ultimos fod, en Y.
 
@@ -72,7 +71,7 @@ def update(adri):         # Creamos una funcion para actualizar nuestra grafica
     ax.bar(1, Alumnos[1], facecolor='#1E1EB2', width=0.5)     #Trazamos la barra de Pepe actualizado
     ax.text(0.55, Alumnos[0]+0.02, '%.2f' %Alumnos[0], ha='center', va='bottom') #Escribimos el valor correspondiente al valor del Slider Ana
     ax.text(1.25, Alumnos[1]+0.02, '%.2f' %Alumnos[1], ha='center', va='bottom') #Escribimos el valor del Slider Pepe
-    ax.axis([0.1, 1.7, 0.0, 10.15])                                              #Mantenemos las mismas dimensiones de la gráfica
+    ax.axis([0.01, 1.7, 0.0, 10.15])                                              #Mantenemos las mismas dimensiones de la gráfica
     ax.set_title('Bar Plot con Sliders', fontsize=25, fontweight='bold')         #Repetimos el titulo
     ax.set_xlabel('Casos')          # Repetimos el nombre del eje de las abscisas
     ax.set_ylabel('Frecuencias')    # Repetimos el nombre del eje de las ordenadas
